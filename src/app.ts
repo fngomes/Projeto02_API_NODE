b93e4680a29527bb39a41b4916ab7870065f7c6e
Felipe Nascimento Gomes <fngomes@stefanini.com>
2023-04-21 21:36:19 -0300

    projeto 02 api node

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
+import { transactions } from './routes/transactions'
+
+export const app = fastify()
+
+app.register(cookie)
+
+app.register(transactions, {
+  prefix: 'transactions',
+})
